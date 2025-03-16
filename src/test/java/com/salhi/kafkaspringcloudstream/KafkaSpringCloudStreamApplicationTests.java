edb8315dce024274bdf003c3240f7afc4739efdb
ms03831 <ms03831@st.habib.edu.pk>
2025-03-16 21:25:52 +0000

    Initial commit

---
+++ src/test/java/com/salhi/kafkaspringcloudstream/KafkaSpringCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salhi.kafkaspringcloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringCloudStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
